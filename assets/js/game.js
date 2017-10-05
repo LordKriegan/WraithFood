379727a292fae473a3ad0095ec741ac710271440
Qamar Stationwala <YoPappy0@gmail.com>
2017-10-05 05:43:35 -0700

    user can now input letters and js checks to see if it is in string and respectively replaces blanks with the letter

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@ var game = {
     losses: 0,
     currentWord: "",
     wordHTML: "",
+    guessedLets: "",
     setWord: function() {
         game.currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
         for (var i = 0; i < game.currentWord.length; i++) {
@@ -12,31 +13,45 @@ var game = {
         console.log(game.currentWord);
     },
     endGame: function() {
-    	console.log("test");
-    },    
+        console.log("This function will end the current game, reset gameboard, and update scoreboard.");
+    },
     checkLet: function(letter) {
-    	for (i = 0; i < game.currentWord.length; i++) {
-    		if (game.currentWord.charAt(i) === letter) {
-    			game.wordHTML = game.wordHTML.substr(0, i) + letter + game.wordHTML.substr(i + 1);
-    		}
-    	}
-    	document.getElementById("word").innerHTML = game.wordHTML;
-    	if (!(game.wordHTML.includes("_"))) {
-    		game.endGame();
-    	}
+        //if letter isnt in word update gamestate accordingly
+        if (!(game.currentWord.includes(letter))) {
+            console.log("This if statement needs to update guesses remaining, and change picture");
+        }
+        //just end the current function if they already guessed this letter
+        else if (game.guessedLets.includes(letter)) {
+        	return;
+        }
+        //otherwise update blanks to correctly guessed letters 
+        else {
+            for (i = 0; i < game.currentWord.length; i++) {
+                if (game.currentWord.charAt(i) === letter) {
+                    game.wordHTML = game.wordHTML.substr(0, i) + letter + game.wordHTML.substr(i + 1);
+                }
+            }
+        }
+        //add letter to list of guessed letters
+        game.guessedLets += letter + " ";
+        //update page to reflect current state of word and letters guessed
+        document.getElementById("lettersGuessed").innerHTML = game.guessedLets;
+        document.getElementById("word").innerHTML = game.wordHTML;
+        if (!(game.wordHTML.includes("_"))) {
+            game.endGame();
+        }
     }
 };
 
 window.onload = function() {
     game.setWord();
-    document.onkeyup = function (event) {
-    	//even though this game is designed for only lower case letters,
-    	//onkeyup only returns ascii code for upper case letters,
-    	//regardless of what user intended to input. Thus,
-    	//we check for upper A and Z instead of lower.
-    	if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
-    		game.checkLet(event.key);
-
-    	}
+    document.onkeyup = function(event) {
+        //even though this game is designed for only lower case letters,
+        //onkeyup only returns ascii code for upper case letters,
+        //regardless of what user intended to input. Thus,
+        //we check for upper A and Z instead of lower.
+        if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
+            game.checkLet(event.key);
+        }
     };
 };
