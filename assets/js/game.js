157ea3a1001174be9b63aec87c9001733ed38e9d
Qamar Stationwala <YoPappy0@gmail.com>
2017-10-05 05:41:45 -0700

    user can now input letters and js checks to see if it is in string and respectively replaces blanks with the letter

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,42 @@
 var game = {
     wins: 0,
     losses: 0,
-    currentWord: "wraithfood",
+    currentWord: "",
+    wordHTML: "",
     setWord: function() {
-        console.log("setWord triggered\n==============");
         game.currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
-        var html = "";
         for (var i = 0; i < game.currentWord.length; i++) {
-            html += "_ ";
+            game.wordHTML += "_";
         }
-        document.getElementById("word").innerHTML = html;
+        document.getElementById("word").innerHTML = game.wordHTML;
+        console.log(game.currentWord);
+    },
+    endGame: function() {
+    	console.log("test");
+    },    
+    checkLet: function(letter) {
+    	for (i = 0; i < game.currentWord.length; i++) {
+    		if (game.currentWord.charAt(i) === letter) {
+    			game.wordHTML = game.wordHTML.substr(0, i) + letter + game.wordHTML.substr(i + 1);
+    		}
+    	}
+    	document.getElementById("word").innerHTML = game.wordHTML;
+    	if (!(game.wordHTML.includes("_"))) {
+    		game.endGame();
+    	}
     }
 };
 
 window.onload = function() {
     game.setWord();
-    console.log(game.currentWord + "\n==============");
-}
+    document.onkeyup = function (event) {
+    	//even though this game is designed for only lower case letters,
+    	//onkeyup only returns ascii code for upper case letters,
+    	//regardless of what user intended to input. Thus,
+    	//we check for upper A and Z instead of lower.
+    	if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
+    		game.checkLet(event.key);
+
+    	}
+    };
+};
