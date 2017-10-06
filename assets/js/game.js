d3ee9a4a27cedd8f6653eaf00879c38872367d0a
Qamar Stationwala <yopappy0@gmail.com>
2017-10-05 19:10:28 -0700

    finished most of game, only thing left is reset and instructions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,49 @@
 var game = {
     wins: 0,
     losses: 0,
+    guessesLeft: 9,
     currentWord: "",
     wordHTML: "",
     guessedLets: "",
+    resetBoard: function() {
+        game.setWord();
+        game.guessesLeft = 9;
+        game.guessedLets = ""
+        document.getElementById("mainPic").src = "assets/images/wrong9.png";
+        document.getElementById("lettersGuessed").innerHTML = "";
+        document.getElementById("guessesLeft").innerHTML = "8";
+    },
     setWord: function() {
         game.currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
+        game.wordHTML = "";
         for (var i = 0; i < game.currentWord.length; i++) {
             game.wordHTML += "_";
         }
         document.getElementById("word").innerHTML = game.wordHTML;
         console.log(game.currentWord);
     },
-    endGame: function() {
-        console.log("This function will end the current game, reset gameboard, and update scoreboard.");
+    endGame: function(isGameWon) {
+        if (isGameWon) {
+            game.wins++;
+            document.getElementById("wins").innerHTML = game.wins;
+        } else {
+            game.losses++;
+            document.getElementById("losses").innerHTML = game.losses;
+        }
+        game.resetBoard();
     },
     checkLet: function(letter) {
         //if letter isnt in word update gamestate accordingly
-        if (!(game.currentWord.includes(letter))) {
-            console.log("This if statement needs to update guesses remaining, and change picture");
+        if (!(game.currentWord.includes(letter)) && !(game.guessedLets.includes(letter))) {
+            if (game.guessesLeft > 0) {
+                game.guessesLeft--;
+                document.getElementById("mainPic").src = "assets/images/wrong" + game.guessesLeft + ".png";
+                document.getElementById("guessesLeft").innerHTML = game.guessesLeft - 1;
+            }
         }
         //just end the current function if they already guessed this letter
         if (game.guessedLets.includes(letter)) {
-        	return;
+            return;
         }
         //otherwise update blanks to correctly guessed letters 
         else {
@@ -47,12 +68,22 @@ window.onload = function() {
         //onkeyup only returns ascii code for upper case letters,
         //regardless of what user intended to input. Thus,
         //we check for upper A and Z instead of lower.
+
         if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
             game.checkLet(event.key);
         }
+        if (game.guessesLeft === 1) {
+            game.guessesLeft = 0;
+            document.getElementById("word").innerHTML = "Game Over! Press any key to continue!";
+        }
+        else if (game.guessesLeft === 0) {
+            game.endGame(false);
+            return;
+        }
         //if wordhtml is same as currentword end the game
         if (game.wordHTML === game.currentWord) {
-            game.endGame();
+            game.endGame(true);
         }
+
     };
 };
