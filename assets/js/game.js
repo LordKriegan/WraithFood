0362cb3393618791074a1a937a02d33557f65179
Qamar Stationwala <YoPappy0@gmail.com>
2017-10-06 05:16:11 -0700

    final touches... maybe

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,10 +17,14 @@ var game = {
         game.currentWord = wordlist[Math.floor(Math.random() * wordlist.length)];
         game.wordHTML = "";
         for (var i = 0; i < game.currentWord.length; i++) {
-            game.wordHTML += "_";
+            if (game.currentWord[i] === " ") {
+                game.wordHTML += " ";
+            }
+            else {
+                game.wordHTML += "_";
+            }
         }
         document.getElementById("word").innerHTML = game.wordHTML;
-        console.log(game.currentWord);
     },
     endGame: function(isGameWon) {
         if (isGameWon) {
@@ -33,7 +37,7 @@ var game = {
         game.resetBoard();
     },
     checkLet: function(letter) {
-        //if letter isnt in word update gamestate accordingly
+        //if letter isnt in word or already guessed update gamestate accordingly
         if (!(game.currentWord.includes(letter)) && !(game.guessedLets.includes(letter))) {
             if (game.guessesLeft > 0) {
                 game.guessesLeft--;
@@ -64,26 +68,61 @@ var game = {
 window.onload = function() {
     game.setWord();
     document.onkeyup = function(event) {
+        //if guessesLeft is 0, then that mean game is over and needs to be reset on keypress
+        if (game.guessesLeft === 0) {
+            if (game.wordHTML === game.currentWord) {
+                game.endGame(true);
+            } else {
+                game.endGame(false);
+            }
+            return;
+        }
         //even though this game is designed for only lower case letters,
         //onkeyup only returns ascii code for upper case letters,
         //regardless of what user intended to input. Thus,
         //we check for upper A and Z instead of lower.
-
         if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
             game.checkLet(event.key);
         }
-        if (game.guessesLeft === 1) {
+
+        //show end game screen but do not reset board yet
+        //set guessesLeft to 0, then when another key is pressed, reset board (see above)
+        if (game.wordHTML === game.currentWord) {
             game.guessesLeft = 0;
-            document.getElementById("word").innerHTML = "Game Over! Press any key to continue!";
-        }
-        else if (game.guessesLeft === 0) {
-            game.endGame(false);
+            var gameAudio = document.createElement("audio");
+            gameAudio.src = "assets/audio/winner.mp3";
+            gameAudio.play();
+            document.getElementById("mainPic").src = "assets/images/winner.png";
+            document.getElementById("word").innerHTML = "Congratulations! You got it! Press any key to continue!";
             return;
         }
-        //if wordhtml is same as currentword end the game
-        if (game.wordHTML === game.currentWord) {
-            game.endGame(true);
+        if (game.guessesLeft === 1) {
+            game.guessesLeft = 0;
+            var gameAudio = document.createElement("audio");
+            gameAudio.src = "assets/audio/loser.mp3";
+            gameAudio.play();
+            //losing picture is taken care of at checkLet (wrong1.png)
+            document.getElementById("word").innerHTML = "Game Over! Press any key to continue!";
+            return;
         }
 
+    };
+
+    //show and hide instructions
+    document.getElementById("btnInstr").onmouseup = function() {
+        document.getElementById("instrOverlay").style.display = "block";
+    };
+    document.getElementById("instrOverlay").onmouseup = function() {
+        document.getElementById("instrOverlay").style.display = "none";
+    };
+    //reset entire game
+    document.getElementById("btnReset").onmouseup = function() {
+        if (confirm("Are you sure? This will clear all your stats!")) {
+            game.wins = 0;
+            game.losses = 0;
+            document.getElementById("wins").innerHTML = "0";
+            document.getElementById("losses").innerHTML = "0";
+            game.resetBoard();
+        }
     };
 };
